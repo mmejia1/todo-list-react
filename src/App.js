b21eed829676ebb263625777ad76f6fac466e746
marjorie mejia <mm035359@gmail.com>
2021-06-14 17:47:03 -0400

    change in the bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AllTodos from './AllTodos';
 import './App.css';
+const URL = 'https://git.heroku.com/halo-challenge.git/todos';
 
 export default function App() {
   return (
---
+++ src/Bar.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+
+export default function Todo(props) {
+  const { todos } = props;
+  return console.log('hello world', todos);
+}
